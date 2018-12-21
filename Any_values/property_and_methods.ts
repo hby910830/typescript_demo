a071cc0bb3ed001fafe154dbd9a1c4f661c5167e
hanbaoyi <604594231@qq.com>
2018-12-21 16:54:26 +0800

    $

---
+++ Any_values/property_and_methods.ts
@@ -0,0 +1,17 @@
+// 在任意值上访问任何属性都是允许的：
+
+let anyThing: any = 'hello'
+console.log(anyThing.myName);
+console.log(anyThing.myName.firstName);
+
+//也允许调用任何方法：
+
+let anyThing: any = 'Tom';
+anyThing.setName('Jerry');
+anyThing.setName('Jerry').sayHello();
+anyThing.myName.setFirstName('Cat');
+
+
+/*
+*  可以认为，声明一个变量为任意值之后，对它的任何操作，返回的内容的类型都是任意值。
+ */
