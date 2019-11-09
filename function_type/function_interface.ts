ae5f340df35376940a52e2678df11ba0e9e95d35
hanbaoyi <604594231@qq.com>
2019-11-09 14:37:16 +0800

    function_interface

--- function_type/function_interface.ts
+++ function_type/function_interface.ts
@@ -1,9 +1,15 @@
-interface seachFunc {
-    (source: string, subString: string): boolean
+//声明一个接口
+interface searchFunc {
+  //下面这个是定义了这个接口的参数为两个 string 类型，返回值为 boolean 类型
+  (source: string, subString: string): boolean
 }
 
-let mySearch: seachFunc
+//声明一个引用 上面 接口类型的变量
+let mySearch: searchFunc
 
+//这里的参数可以写成与接口中的参数不一样的名字，这里的参数是针对的类型检查而不是值检查
 mySearch = function (source: string, subString: string) {
-    return source.search(subString) !== 1
+  return source.search(subString) !== 1 //search 是 js 中查询字符串的方法 如果没有的话返回的是 -1
 }
+
+console.log(mySearch('hby', 'h'));
