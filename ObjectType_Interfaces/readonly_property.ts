d82298f4a97ffcbd3e1dddb710c30a66531b4f0c
hanbaoyi <604594231@qq.com>
2019-11-11 17:58:07 +0800

    readonly_property

--- ObjectType_Interfaces/readonly_property.ts
+++ ObjectType_Interfaces/readonly_property.ts
@@ -12,5 +12,4 @@ let Tom:Person = {
     age: 12,
     gender: 'female'
 }
-Tom.id = 12
-// error TS2540: Cannot assign to 'id' because it is a read-only property.   Tom.id = 12
+// Tom.id = 12  // error TS2540: Cannot assign to 'id' because it is a read-only property.   Tom.id = 12
